2e3d147eafcd8c5c33d28e84f69a6bfff29dbdda
Alvaro <alvaroandradesantos2009@gmail.com>
2026-03-18 10:29:28 -0300

    2 - programa de operações

---
+++ DescontoCompra.py
@@ -0,0 +1,5 @@
+valor = float(input("Digite o valor de sua compra:\n"))
+if valor > 100.00:
+    print("Sua compra terá desconto!")
+else:
+    print("Sua compra não terá desconto")
---
+++ OperacoesNumeros.py
@@ -0,0 +1,3 @@
+n1 = int(input("Digite um número:\n"))
+n2 = int(input("Digite outro número:\n"))
+print(" Soma: ", n1 + n2, "\n Subtração: ", n1 - n2, "\n Multiplicação: ", n1 * n2, "\n Divisão: ", n1 / n2)
