0342cff214ad754ed30022d417c6814097fb5cb5
Alvaro <alvaroandradesantos2009@gmail.com>
2026-04-10 08:58:22 -0300

    Exercicio 14 - soma com funcao

---
+++ somafuncao.py
@@ -0,0 +1,7 @@
+def soma(n1, n2):
+    return n1+n2
+
+numero1 = float(input("Insira o primeiro número a somar:\n"))
+numero2 = float(input("Insira o segundo número a somar:\n"))
+
+print(soma(numero1, numero2))
